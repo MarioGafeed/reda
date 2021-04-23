4ba25a73f1299d4a7e67cab8d8b0e4fa0dfcb6c7
MarioGafeed <jesushostingme@gmail.com>
2021-04-23 22:03:04 +0200

    Try fix setting
    try fix setting

--- resources/views/backend/theme/includes/sidebar.blade.php
+++ resources/views/backend/theme/includes/sidebar.blade.php
@@ -214,30 +214,7 @@
 
 
              <!-- Add Site Seeting   (Mario Added) -->
-             <li class="heading">
-                 <h3 class="uppercase">{{ trans('main.settings') }}</h3>
-             </li>
-
-             <li class="nav-item  {{ active_route('setting.*') }}">
-                 <a href="javascript:;" class="nav-link nav-toggle">
-                     <i class="icon-users"></i>
-                     <span class="title">{{ trans('main.settings') }}</span>
-                     <span class="arrow"></span>
-                 </a>
-                 <ul class="sub-menu">
-                     <li class="nav-item {{ active_route('settings.edit') }}">
-                         <a href="{{ route('settings.edit', 1) }}" class="nav-link ">
-                             <span class="title">{{ trans('main.edit') }} {{ trans('main.settings') }}</span>
-                         </a>
-                     </li>
-                     <li class="nav-item {{ active_route('settings.show') }}">
-                         <a href="{{ route('settings.show', 1) }}" class="nav-link ">
-                             <span class="title">{{ trans('main.show-all') }} {{ trans('main.settings') }}</span>
-                         </a>
-                     </li>
-
-                 </ul>
-             </li>
+             
 
         </ul>
         <!-- END SIDEBAR MENU -->
